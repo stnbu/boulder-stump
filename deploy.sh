a375eaea4447331e2635a5ef416b0d7d97e6e0dd
Mike Burr <mburr@unintuitive.org>
2021-09-13 07:21:33 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,12 @@ REMOTE_DIR="/home/mburr/git/unintuitive.org/www/boulder-stump"
 
 cd $(dirname $0)
 
-ORG_FILES=$(git status --short --untracked-files=no --porcelain | grep '\.org$' | cut -d ' ' -f 3 | xargs ls -1t)
+#ORG_FILES=$(git status --short --untracked-files=no --porcelain | grep '\.org$' | cut -d ' ' -f 3 | xargs ls -1t)
+ORG_FILES=001.org
 
 for ORG_FILE in $ORG_FILES ; do
     OUT_FILE=$(basename $ORG_FILE ".org").html
+    echo "Writing $OUT_FILE ..."
     pandoc -s $ORG_FILE -o $OUT_FILE
     #emacs "$ORG_FILE" --batch -f org-html-export-to-html --kill >/dev/null 2>&1
 done
