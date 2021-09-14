02ad8ed9659b2f5bac854608650ec494a897921e
Mike Burr <mb@unintuitive.org>
2021-09-14 08:29:40 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -23,12 +23,12 @@ cp -f index.css target/html/ # um.
 rsync -xa target/html/ pu:"$REMOTE_DIR"/
 
 echo "Web server updated."
-
+# > /dev/null 2>&1
 # whatamess+
 if git status -s | grep -q ^ ; then
-    git add *.org *.sh index.css README.md > /dev/null || echo "no add"
+    git add *.org *.sh index.css README.md || echo "no add"
     git commit -qm 'deploy-bot' || echo "no commit"
-    git push > /dev/null || echo "no push"
+    git push || echo "no push"
     echo "Changes pushed."
 else
     echo "No changes found."
