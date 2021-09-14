8fd83ff082288f1641f8e17f0728b77375ff3e29
Mike Burr <mb@unintuitive.org>
2021-09-14 08:28:07 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -27,8 +27,8 @@ echo "Web server updated."
 # whatamess+
 if git status -s | grep -q ^ ; then
     git add *.org *.sh index.css README.md > /dev/null || echo "no add"
-    git commit -qm 'deploy-bot' > /dev/null || echo "no commit"
-    git push || echo "no push"
+    git commit -qm 'deploy-bot' || echo "no commit"
+    git push > /dev/null || echo "no push"
     echo "Changes pushed."
 else
     echo "No changes found."
