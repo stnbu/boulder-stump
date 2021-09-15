90fcebed90bfdba8bb8db93aefc4c2ea3d49346b
Mike Burr <mb@unintuitive.org>
2021-09-15 07:29:13 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cp -f index.css target/html/ ; echo "Copied CSS."
 rsync -xa target/html/ target/pdf/ pu:"$REMOTE_DIR"/ # dumpet together
 echo "Web server updated."
 
-if git status -s | grep -q ^ ; then
+if git status --untracked-files=no --short | grep -q ^ ; then
     git add book.org *.sh index.css README.md || echo "no add"
     git commit -qm 'deploy-bot' || echo "no commit"
     git push 2> /dev/null || echo "no push"
