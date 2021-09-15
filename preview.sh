cd05ddc32197e838176ea1d265979db35d2ddb43
Mike Burr <mb@unintuitive.org>
2021-09-15 10:29:08 -0600

    preview

---
+++ preview.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ue
+
+cd $(dirname $0)
+
+mkdir -p preview/pdf preview/html
+
+PATH="/usr/local/texlive/2021/bin/universal-darwin:$PATH"
+sed -E 's/:noexport://' gog.org > preview/pdf/.gog.org ## using stdin instead of file broke! somehow.
+pandoc --toc -s preview/pdf/.gog.org -o preview/pdf/gog.pdf ; echo "Created PDF."
+open preview/pdf/gog.pdf
+
+problem() {
+    echo "PROBLEM! Job had non-zero status: ${1}"
+}
+
+if git status --untracked-files=no --short | grep -q ^ ; then
+    git add gog.org *.sh index.css README.md  || problem "git add"
+    git commit -qm 'deploy-bot'               || problem "git commit"
+    git push 2> /dev/null                     || problem "git push"
+    echo "Changes pushed."
+else
+    echo "No changes found."
+fi
