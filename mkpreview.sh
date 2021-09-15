0051b6ab943d805d25db94f1139f7f666af4a667
Mike Burr <mb@unintuitive.org>
2021-09-15 10:28:31 -0600

    deploy-bot

--- mkpreview.sh
+++ mkpreview.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 mkdir -p preview/pdf preview/html
 
 PATH="/usr/local/texlive/2021/bin/universal-darwin:$PATH"
-sed -E 's/:noexport://' gog.org > preview/pdf/.gog.org
+sed -E 's/:noexport://' gog.org > preview/pdf/.gog.org ## using stdin instead of file broke! somehow.
 pandoc --toc -s preview/pdf/.gog.org -o preview/pdf/gog.pdf ; echo "Created PDF."
 open preview/pdf/gog.pdf
 
