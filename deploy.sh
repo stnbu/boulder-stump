4d097528c52ea516639c1d6bdf84c9dea2d81590
Mike Burr <mb@unintuitive.org>
2021-09-15 07:34:11 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,14 @@ cp -f index.css target/html/ ; echo "Copied CSS."
 rsync -xa target/html/ target/pdf/ pu:"$REMOTE_DIR"/ # dumpet together
 echo "Web server updated."
 
+problem() {
+    echo "PROBLEM! Job had non-zero status: ${1}"
+}
+
 if git status --untracked-files=no --short | grep -q ^ ; then
-    git add book.org *.sh index.css README.md || echo "no add"
-    git commit -qm 'deploy-bot' || echo "no commit"
-    git push 2> /dev/null || echo "no push"
+    git add book.org *.sh index.css README.md || problem "git add"
+    git commit -qm 'deploy-bot' || problem "git commit"
+    git push 2> /dev/null || problem "git push"
     echo "Changes pushed."
 else
     echo "No changes found."
