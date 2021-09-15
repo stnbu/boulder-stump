c612adf8a57ad3f46d0df994b1fe47fbca92977f
Mike Burr <mb@unintuitive.org>
2021-09-15 07:26:39 -0600

    deploy-bot

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ cd $(dirname $0)
 mkdir -p target/html target/pdf
 
 PATH="/usr/local/texlive/2021/bin/universal-darwin:$PATH"
-pandoc --toc -s book.org -o target/html/book.html
-pandoc --toc -s book.org -o target/pdf/book.pdf
-pandoc -s README.md -o target/html/index.html
-cp -f index.css target/html/ # um.
+pandoc --toc -s book.org -o target/pdf/book.pdf ; echo "Created PDF."
+pandoc --toc -s book.org -o target/html/book.html ; echo "Created HTML."
+pandoc -s README.md -o target/html/index.html ; echo "Created index."
+cp -f index.css target/html/ ; echo "Copied CSS."
 
 rsync -xa target/html/ target/pdf/ pu:"$REMOTE_DIR"/ # dumpet together
 echo "Web server updated."
